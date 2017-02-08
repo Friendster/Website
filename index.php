77f07b4a72e36d27442571a2b1a7517c4a71e172
Cristiana Man <Cristiana Man>
2017-02-08 04:25:14 +0100

    progress on login. Query db find existing user, verify pass

--- index.php
+++ index.php
@@ -5,4 +5,4 @@
  * Date: 2/3/2017
  * Time: 20:35
  */
-include "form.php";
+include "login.php";
---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+
+include "header.php";
+include "db.php";
+
+$usr = "";
+$pass = "";
+$usrErr = "";
+$passErr = "";
+$loginMsg = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["usr"])) {
+        $usrErr = "Required";
+    } else {
+        $usr = sanitize_input($_POST["usr"]);
+    }
+
+    if (empty($_POST["pass"])) {
+        $passErr = "Required";
+    } else {
+        $pass = sanitize_input($_POST["pass"]);
+    }
+
+    // If user input is ok so far then QUERY db
+    if ($usrErr === "" && $passErr === "") {
+        $loginMsg = "FAILED LOGIN!";
+
+        // Connect to db
+        $conn = connect_to_db();
+
+        //http://php.net/manual/en/mysqli-stmt.get-result.php
+
+        $sql = "SELECT * FROM `adrielsi_crisdb`.`user` WHERE `username`=$usr";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            echo $row["username"], $row["pass"];
+
+            if (password_verify($pass, $row["pass"])) {
+                $loginMsg = "SUCCESSFUL LOGIN!";
+            } else {
+                $passErr = "Wrong password";
+            }
+
+        } else {
+            $usrErr = "Inexistent user";
+        }
+
+        $conn->close();
+    }
+}
+
+function sanitize_input($data) {
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+<div class="row align-items-center justify-content-center">
+    <div class="my-login-box col-md-6 col-lg-4">
+        <h1>Enter Wonderland!</h1>
+
+        <?php if(!empty($loginMsg)) echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> $loginMsg</div>"; ?>
+
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" autocomplete="off">
+            <div class="form-group <?php if(!empty($usrErr)) echo "has-danger";?>">
+                <label class="form-control-label" for="usr">Unicorn name</label>
+                <input type="text" class="form-control <?php if(!empty($usrErr)) echo "form-control-danger";?>" id="usr" name="usr" value="<?php echo "$usr";?>">
+                <div class="form-control-feedback" <?php if(empty($usrErr)) echo "hidden";?>>
+                    <?php echo $usrErr;?>
+                </div>
+            </div>
+            <div class="form-group <?php if(!empty($passErr)) echo "has-danger";?>">
+                <label class="form-control-label" for="pass">Secret Magical Password</label>
+                <input type="password" class="form-control <?php if(!empty($passErr)) echo "form-control-danger";?>" id="pass" name="pass" value="<?php echo "$pass";?>">
+                <div class="form-control-feedback" <?php if(empty($passErr)) echo "hidden";?>>
+                    <?php echo $passErr;?>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+
+    </div>
+</div>
+
+<?php include "footer.php";?>

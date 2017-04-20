021a425ef4a85fe5657a5a4ba7a20137bd0a9fab
Cristiana Man <man.cristiana1@gmail.com>
2017-04-20 21:19:27 +0200

    Fixed config import

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 $config = new stdClass();
 $config -> host = 'localhost';
+$config -> database_host = 'localhost';
 $config -> username = 'root';
 $config -> password = 'db#2IsAw3s0me';
 $config -> database = 'friendster';
-$config -> port = 80;
-$config -> path = '';
+$config -> port = '';
+$config -> path = '/Friendster';
 
 return $config;
 ?>
--- include/header.php
+++ include/header.php
@@ -1,3 +1,24 @@
+<?php
+session_start();
+
+$customPath = (empty($_SERVER['HTTPS'])) ? '/Friendster' : '';
+$configs = include($_SERVER['DOCUMENT_ROOT'] . $customPath . '/config.php');
+
+function get_style() {
+    global $configs;
+
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https://' : 'http://';
+    $host = $_SERVER['SERVER_NAME'];
+    $port = (!empty($configs->port)) ? ':' . $configs->port : '';
+    $customPath = ($host == $configs->host) ? $configs->path : '';
+
+    $link = $protocol . $host . $port . $customPath . '/css/mystyle.css';
+
+    return $link;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,26 +28,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo get_style() ?>" type="text/css">
+    <link rel="stylesheet" href=<?php echo get_style();?> type="text/css">
 
 </head>
 <body>
 <div class="container-fluid">
 
-    <?php
-    session_start();
 
-    function get_style()
-    {
-        $configs = include('config.php');
-
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 'https://' : 'http://';
-        $customPath = ($_SERVER['SERVER_NAME'] == $configs->host) ? $configs->path : '';
-        $link = $protocol . $_SERVER['SERVER_NAME'] . $customPath . '/css/mystyle.css';
-
-        return $link;
-    }
-
-    ?>
 
 

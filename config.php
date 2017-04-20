a79b42ed70ac6bf5fdb7a2eed1f88f36d2ed54ba
Boris Ganev <boris.ganev@hotmail.com>
2017-04-20 19:16:27 +0100

    Update Server Config
    Fix to server config file

--- config.php
+++ config.php
@@ -2,7 +2,9 @@
 $config = new stdClass();
 $config -> host = 'localhost';
 $config -> username = 'root';
+$config -> password = 'db#2IsAw3s0me';
 $config -> database = 'friendster';
+$config -> port = 80;
 $config -> path = '/Friendster';
 
 return $config;

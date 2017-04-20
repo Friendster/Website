4724beddd87b908209f0cff04929e5103594e212
Boris Ganev <boris.ganev@hotmail.com>
2017-04-20 19:11:49 +0100

    Server Config
    Editing of config.php for server

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = new stdClass();
+$config -> host = 'localhost';
+$config -> username = 'root';
+$config -> database = 'friendster';
+$config -> path = '/Friendster';
+
+return $config;
+?>

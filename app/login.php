2fa1b51e3344375f51d39457e6d930837201afe9
Cristiana Man <man.cristiana1@gmail.com>
2017-04-20 22:30:09 +0200

    Login refactoring

--- app/login.php
+++ app/login.php
@@ -5,57 +5,57 @@ include "../include/init.php";
 include "googlerecaptcha.php";
 
 
-$user = '';
-$pass = '';
-$userError = '';
-$passError = '';
-$recapchaError = '';
+$email = '';
+$password = '';
+$email_error = '';
+$password_error = '';
+$recaptcha_error = '';
 
-$loginError = '';
+$login_error = '';
 
 
 if (isset($_POST["submit"])) {
 
     $is_user_valid = validate_user($_POST["usr"]);
     $is_pass_valid = validate_pass($_POST["pass"]);
-    $is_recapcha_valid = is_recapcha_valid($_POST["g-recaptcha-response"]);
+    $is_recaptcha_valid = is_recapcha_valid($_POST["g-recaptcha-response"]);
 
-    if (!$is_recapcha_valid) {
-        $recapchaError = "Please validate recapcha";
+    if (!$is_recaptcha_valid) {
+        $recaptcha_error = "Please validate recapcha";
     }
 
     // If user input is ok so far then QUERY db
-    if ($is_user_valid && $is_pass_valid && $is_recapcha_valid) {
-        $db_user = db_get_user($user);
+    if ($is_user_valid && $is_pass_valid && $is_recaptcha_valid) {
+        $db_user = db_get_user($email);
 
         $hashed_pw = $db_user->pass;
-        if (!empty($hashed_pw) && password_verify($pass, base64_decode($hashed_pw))) {
-            $_SESSION["name"] = $user;
+        if (!empty($hashed_pw) && password_verify($password, base64_decode($hashed_pw))) {
+            $_SESSION["name"] = $email;
             $_SESSION["user_id"] = $db_user->id;
             header("Location: ../index.php");
         } else {
-            $loginError = "Username or password is wrong";
+            $login_error = "Username or password is wrong";
         }
     }
 
 }
 
 function validate_user($value) {
-    global $userError, $user;
-    $user = htmlspecialchars($value);
+    global $email_error, $email;
+    $email = htmlspecialchars($value);
 
-    $isValid = !empty($user);
-    $userError = $isValid ? "" : "is required";
+    $isValid = !empty($email);
+    $email_error = $isValid ? "" : "is required";
 
     return $isValid;
 }
 
 function validate_pass($value) {
-    global $passError, $pass;
-    $pass = htmlspecialchars($value);
+    global $password_error, $password;
+    $password = htmlspecialchars($value);
 
-    $isValid = !empty($pass);
-    $passError = $isValid ? "" : "is required";
+    $isValid = !empty($password);
+    $password_error = $isValid ? "" : "is required";
 
     return $isValid;
 }
@@ -66,41 +66,40 @@ function validate_pass($value) {
         <div class="panel-body">
 
 
-            <?php if (!empty($loginError)) echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> $loginError</div>"; ?>
+            <?php if (!empty($login_error)) echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> $login_error</div>"; ?>
 
 
             <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" autocomplete="off">
-<!--                <fieldset>-->
-                    <legend>Enter Wonderland!</legend>
-                    <div class="form-group <?php echo !empty($userError) ? "has-error" : ""; ?>">
-                        <label class="control-label" for="usr">Unicorn
-                            name <?php if (!empty($userError)) echo $passError; ?></label>
-                        <input type="text"
-                               class="form-control <?php if (!empty($userError)) echo "form-control-danger"; ?>"
-                               id="usr"
-                               name="usr" value="<?php echo "$user"; ?>">
-                    </div>
-                    <div class="form-group <?php echo !empty($passError) ? "has-error" : ""; ?>">
-                        <label class="control-label" for="pass">Secret Magical
-                            Password <?php if (!empty($passError)) echo $passError; ?></label>
-                        <input type="password"
-                               class="form-control <?php if (!empty($passError)) echo "form-control-danger"; ?>"
-                               id="pass"
-                               name="pass" value="<?php echo "$pass"; ?>">
-                    </div>
+                <legend>Enter Wonderland!</legend>
+                <div class="form-group <?php echo !empty($email_error) ? "has-error" : ""; ?>">
+                    <label class="control-label" for="usr">Unicorn
+                        name <?php if (!empty($email_error)) echo $password_error; ?></label>
+                    <input type="text"
+                           class="form-control <?php if (!empty($email_error)) echo "form-control-danger"; ?>"
+                           id="usr"
+                           name="usr" value="<?php echo "$email"; ?>">
+                </div>
+                <div class="form-group <?php echo !empty($password_error) ? "has-error" : ""; ?>">
+                    <label class="control-label" for="pass">Secret Magical
+                        Password <?php if (!empty($password_error)) echo $password_error; ?></label>
+                    <input type="password"
+                           class="form-control <?php if (!empty($password_error)) echo "form-control-danger"; ?>"
+                           id="pass"
+                           name="pass" value="<?php echo "$password"; ?>">
+                </div>
 
 
-                    <div class="<?php echo empty($recapchaError) ? "" : "recapcha-error"; ?>">
-                        <?php echo empty($recapchaError) ? "" : "<p class=\"text-danger\">$recapchaError</p>"; ?>
-                        <div class="g-recaptcha" data-sitekey="6LcpFBoUAAAAAOCkYXYqvLNlnrFzXMn3DrSDdHzD"></div>
-                    </div>
+                <div class="<?php echo empty($recaptcha_error) ? "" : "recapcha-error"; ?>">
+                    <?php echo empty($recaptcha_error) ? "" : "<p class=\"text-danger\">$recaptcha_error</p>"; ?>
+                    <div class="g-recaptcha" data-sitekey="6LcpFBoUAAAAAOCkYXYqvLNlnrFzXMn3DrSDdHzD"></div>
+                </div>
 
 
-                    <div class="form-buttons">
-                        <button type="submit" name="submit" class="btn btn-primary">Login</button>
-                        <a class="btn btn-default" href="register.php">Register?</a>
-                    </div>
-<!--                    <fieldset>-->
+                <div class="form-buttons">
+                    <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                    <a class="btn btn-default" href="register.php">Register?</a>
+                </div>
+
             </form>
         </div>
     </div>

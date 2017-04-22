923acf07364939fc8a6c8076bd07366a7a569c7b
Cristiana Man <man.cristiana1@gmail.com>
2017-04-22 13:37:47 +0200

    Added modal

--- app/profile_header.php
+++ app/profile_header.php
@@ -11,12 +11,31 @@
 <div class="container-fluid nopadding cover">
 
 
-<div class="col-md-3">
+    <div class="col-md-3">
         <div class="panel panel-default profile">
             <div class="panel-body">
                 Basic panel
+                <a href="#modal" data-toggle="modal">Open modal</a>
             </div>
         </div>
+    </div>
+
 </div>
 
+<div id="modal" class="modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
 </div>

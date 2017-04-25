7e45970c43a3316f6ba592c67945fce7b7a8431e
EliiseS <selingeliise@gmail.com>
2017-04-25 18:41:35 +0200

    Added session timeout and recreation

--- include/header.php
+++ include/header.php
@@ -1,5 +1,21 @@
 <?php
+//TODO: Should this be in the header or somewhere else? Preferably in a method and a file of its own, but that's problematic(See CSS)
 session_start();
+//Regenerate session every 5 minutes
+if (!isset($_SESSION['CREATED'])) {
+    $_SESSION['CREATED'] = time();
+} else if (time() - $_SESSION['CREATED'] > 300) {
+    // session started more than 5 minutes ago
+    session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+    $_SESSION['CREATED'] = time();  // update creation time
+}
+//Destroy session if last activity was 10 minutes ago
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
+    // last request was more than 10 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 
 $customPath = (empty($_SERVER['HTTPS'])) ? '/Friendster' : '';
 $configs = include($_SERVER['DOCUMENT_ROOT'] . $customPath . '/config.php');

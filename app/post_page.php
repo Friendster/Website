edb4666989d026c0b622d6604d9721a64c3b56b5
Cristiana Man <man.cristiana1@gmail.com>
2017-04-27 15:00:36 +0200

    Updated delete feature

--- app/post_page.php
+++ app/post_page.php
@@ -11,27 +11,50 @@
 
 $user_name = $_SESSION["name"];
 $user_id = $_SESSION["user_id"];
+$posts = db_get_posts();
+
+function find($post_id){
+    global $posts;
+    $post2 = $posts;
+    while ($post2 = $post2->fetch_assoc()) {
+        if($post2['post_id'] == $post_id) {
+            return $post2;
+        }
+    }
+    return null;
+}
+
+function is_post_author($user_id, $post_id) {
+    $post = find($post_id);
+    if($post && $user_id == $post['user_id']) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 if (isset($_POST['submit'])) {
 
     $content = $_POST['content'];
     db_create_post($user_id, $content);
 }
+
 //TODO need post id
 if (isset($_POST["delete"])) {
-
-    $post_id = $_POST['postid'];
-    echo("<script>alert(\"SOMETHING\")</script>post id from delete".$post_id);
-    //db_delete_post($post_id);
+    $post_id = $_POST["id"];
+    if(is_post_author($user_id, $post_id)) {
+//        echo("<script>alert(" . $post_id . ")</script>");
+        //db_delete_post($post_id);
+    }
 }
 
-//TODO: need post content, post id  HELP?
-if (isset($_POST["edit"])) {
-
-    $post_id = $_GET['postid'];
-
-    db_update_post($post_id);
-}
+////TODO: need post content, post id  HELP?
+//if (isset($_POST["edit"])) {
+//
+//    $post_id = $_GET['postid'];
+//
+//    db_update_post($post_id);
+//}
 
 
 ?>
@@ -55,29 +78,28 @@ if (isset($_POST["edit"])) {
 
         <?php
 
-        $posts = db_get_posts();
-        while ($post = $posts->fetch_assoc()) {
-            $editButtons =
 
-                '<div class="form-buttons">' .
-                '<form method="post" >' .
-                '<button name="delete" type="delete" postid=' . $post['post_id'] . ' class="btn btn-danger">Delete</button>' .
-                '<textarea  name="content" class="form-control" rows="3"
-                              id="textArea"></textarea>' .
-                '</form>' .
-                '<form method="edit" >' .
-                '<button name="edit" type="edit" postid=' . $post['post_id'] . ' class="btn btn-primary">Edit</button>' .
-                '</form>' .
-                '</div>';
+        while ($post = $posts->fetch_assoc()) {
+//            $link = urlencode($_SERVER["PHP_SELF"]) . "?deleteid=" . urlencode($post['post_id']);
+//            $link = "?deleteid=" . urlencode($post['post_id']);
+            $delete_button = ($user_name == $post['author']) ?
+
+//                '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">
+                '<form method="post" action="#">
+                    <input type="hidden" name="id" value="' . htmlspecialchars($post['post_id']) . '" />
+                    <div class="form-group">
+                        <button name="delete" type="submit" class="btn btn-danger">Delete</button>
+                    </div>
+                </form>' : "";
 
 
             echo
                 '<div class="panel panel-default">' .
-                '<div class="panel-heading">' . htmlspecialchars($post['author']) . '</div>' .
+                '<div class="panel-heading">' . htmlspecialchars($post['author']) .  htmlspecialchars($post['post_id']) .'</div>' .
                 '<div class="panel-body">' .
                 htmlspecialchars($post['content']) . '<br />' .
                 htmlspecialchars($post['date']) .
-                ($user_name == $post['author'] ? ($editButtons) : "") .
+                $delete_button .
                 '</div>' .
                 '</div>';
 

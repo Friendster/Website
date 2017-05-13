142c71ccdcb9905ef94a320a7cffd393a0d8a766
Cristiana Man <man.cristiana1@gmail.com>
2017-05-13 13:09:48 +0200

    Retrieve profile information on login

--- app/login.php
+++ app/login.php
@@ -1,13 +1,19 @@
 <?php
 
 
-function login($email, $id)
+function login($profile)
 {
     session_start();
 
     // TODO refactor session name -> email
-    $_SESSION["name"] = $email;
-    $_SESSION["user_id"] = $id;
+
+    // TODO encapsulate session into a session object
+    $_SESSION["name"] = $profile->email;
+    $_SESSION["user_id"] = $profile->id;
+    $_SESSION["profile_picture_name"] = $profile->profile_picture_name;
+
+    // TODO add other profile details
+
     set_location_to_root();
 }
 

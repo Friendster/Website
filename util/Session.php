a1f2fd0c043d0b8cd337ff66a94a1fe3471a02ce
Cristiana Man <man.cristiana1@gmail.com>
2017-05-15 02:21:27 +0200

    Moved some files to the util folder

--- util/Session.php
+++ util/Session.php
@@ -65,14 +65,3 @@ class Session {
         echo Properties::LAST_ACTIVITY . ' = ' . $this->get(Properties::LAST_ACTIVITY) . '<br>';
     }
 }
-
-// TODO move to a different file
-abstract class Properties {
-    const ID = 'ID';
-    const EMAIL = 'EMAIL';
-    const NAME = 'NAME';
-    const PROFILE_PICTIRE_NAME = 'PROFILE_PICTIRE_NAME';
-    const TOKEN = 'TOKEN';
-    const CREATED = 'CREATED';
-    const LAST_ACTIVITY = 'LAST_ACTIVITY';
-}

815e99e18c8a035cca6461e6ff6a21f0a99c75a2
Cristiana Man <man.cristiana1@gmail.com>
2017-05-15 02:21:01 +0200

    Added ValidationHandler

---
+++ util/ValidationHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mancr
+ * Date: 15-May-17
+ * Time: 1:31
+ */
+class ValidationHandler {
+
+    public static function validateRequired($value) {
+        $error = '';
+        if (empty($value)) {
+            $error = 'is required';
+        }
+
+        return $error;
+    }
+
+    public static function validatePassword($password) {
+        $error = ValidationHandler::validateRequired($password);
+
+        if (!ValidationHandler::isValid($error)) {}
+        elseif(strlen($password) < '8') {
+            $error = 'must contain at least 8 characters!';
+        } elseif (!preg_match('#[0-9]+#', $password)) {
+            $error = 'must contain at least 1 number!';
+        } elseif (!preg_match('#[A-Z]+#', $password)) {
+            $error = 'must contain at least 1 capital letter!';
+        } elseif (!preg_match('#[a-z]+#', $password)) {
+            $error = 'must contain at least 1 lowercase letter!';
+        }
+
+        return $error;
+    }
+
+    public static function validateEmail($email) {
+        $error = ValidationHandler::validateRequired($email);
+
+        if (!ValidationHandler::isValid($error)) {}
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = 'is NOT valid!';
+        } // Check if user exists
+        elseif (!empty($user = db_get_user($email)->id)) {
+            $error = 'has been registered';
+        }
+
+        return $error;
+    }
+
+    public static function validateRecaptcha($value) {
+        $error = '';
+        if (!is_recapcha_valid($value)) {
+            $error = 'Please validate recapcha';
+        }
+
+        return $error;
+    }
+
+    public static function isValid($error) {
+        return empty($error);
+    }
+
+
+}

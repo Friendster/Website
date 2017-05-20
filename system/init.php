b928d6658d3b4256470cd9ecd53b0e5c385b8884
EliiseS <selingeliise@gmail.com>
2017-05-21 00:51:20 +0200

    Fix issue with merging incorrectly

--- system/init.php
+++ system/init.php
@@ -2,6 +2,8 @@
 /**
  * This file should use the db, create the session, config,
  */
+include "../app/entities/User.php";
+include "../app/entities/Post.php";
 
 include "../system/session/Properties.php";
 include "../system/session/SessionManager.php";
@@ -27,9 +29,6 @@ include "Database.php";
 
 
 
-include "../app/image_upload.php";
-
-
 function navigate_to($query = "")
 {
     $root_location = (Config::$host != "localhost") ? "/" : "index.php". $query;
@@ -38,6 +37,12 @@ function navigate_to($query = "")
 
 include "RecaptchaManager.php";
 include "ValidationManager.php";
+include "ImageManager.php";
+
+
+include "../app/controller/AppController.php";
+include "../app/model/AppModel.php";
+include "../app/view/AppView.php";
 
 include "../app/controller/LoginController.php";
 include "../app/model/LoginModel.php";
@@ -50,3 +55,7 @@ include "../app/view/RegisterView.php";
 include "../app/controller/ProfileController.php";
 include "../app/model/ProfileModel.php";
 include "../app/view/ProfileView.php";
+
+include "../app/controller/PostController.php";
+include "../app/model/PostModel.php";
+include "../app/view/PostView.php";
